12acd5fc25efa6d8129481fd4d66c8c9de8e6784
marriedsun <marriedsun@gmail.com>
2025-11-03 12:46:29 +0900

    버튼기능활성화

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  function down(){
+    setCount(count-1);
+  } 
+  function reset(){
+    setCount(0);
+  } 
+  function up(){
+    setCount(count+1);
+  } 
   return (
     <div >
-      <input type="button" value="-" />
-      <input type="button" value="0" />
-      <input type="button" value="+" />
-      <span>0</span>
+      <input type="button" value="-" onClick={down} />
+      <input type="button" value="0" onClick={reset} />
+      <input type="button" value="+" onClick={up} />
+      <span>{count}</span>
     </div>
   );
 }
