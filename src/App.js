b59f76ed8ef414ed2cff1862d6326fd7bc191b3b
marriedsun <marriedsun@gmail.com>
2025-11-03 12:40:43 +0900

    버튼생성

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React from "react";
 function App() {
   return (
     <div >
-      
-     
+      <input type="button" value="-" />
+      <input type="button" value="0" />
+      <input type="button" value="+" />
+      <span>0</span>
     </div>
   );
 }
