aeecc0b7cde6a4f1f69c82035358169e1135b358
marriedsun <marriedsun@gmail.com>
2025-11-03 13:05:43 +0900

    usestate와 usereducer를 이용한 응용예제

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React, {useReducer} from "react";
+import React, {useReducer, useState} from "react";
 
 function App() {
+  const [number, setNumber] = useState(1);
   function counReducer(oldCount, action){
-    if(action === 'UP'){
-      return oldCount + 1;
-    } else if(action === 'DOWN') {
-      return oldCount - 1;
-    } else if(action === 'RESET'){
+    if(action.type === 'UP'){
+      return oldCount + action.number;
+    } else if(action.type === 'DOWN') {
+      return oldCount - action.number;
+    } else if(action.type === 'RESET'){
       return 0;
     }
   }
   const [count, countDispatch] = useReducer(counReducer, 0);
   function down(){
-    countDispatch ('DOWN');
+    countDispatch ({ type: 'DOWN', number: number });
   } 
   function reset(){
-    countDispatch('RESET');
+    countDispatch({ type: 'RESET', number: number });
   } 
   function up(){
-    countDispatch('UP');
+    countDispatch({ type: 'UP', number: number });
   } 
+  function changeNumber(event){
+    setNumber(Number(event.target.value));
+  }
   return (
     <div >
       <input type="button" value="-" onClick={down} />
       <input type="button" value="0" onClick={reset} />
       <input type="button" value="+" onClick={up} />
+      <input type="text" value={number} onChange={changeNumber} />
       <span>{count}</span>
     </div>
   );
